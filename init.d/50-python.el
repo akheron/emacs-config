e9fcc4b6d035b545fa4db376e93324c7221e8fa8
Petri Lehtinen <petri.lehtinen@inoi.fi>
2010-02-25 09:34:42 +0200

    python-mode: Turn off auto-fill-mode
    For some reason, sometimes python-mode turns on auto-fill-mode
    automatically.

--- init.d/50-python.el
+++ init.d/50-python.el
@@ -7,6 +7,7 @@
       (define-key python-mode-map (kbd "C-c C-;") 'python-shift-left)
       (define-key python-mode-map (kbd "C-c C-:") 'python-shift-right)
       (define-key python-mode-map (kbd "C-c C-c") 'comment-region)
-      (define-key python-mode-map (kbd "C-c C-u") 'uncomment-region)))
+      (define-key python-mode-map (kbd "C-c C-u") 'uncomment-region)
+      (turn-off-auto-fill)))
 
 (add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
