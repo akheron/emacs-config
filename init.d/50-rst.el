0fd185f89e1e147b12a15ede7ad7f9c49224fcbc
Petri Lehtinen <petri@digip.org>
2010-03-17 20:26:25 +0200

    Disable font-lock-mode in rst-mode

--- init.d/50-rst.el
+++ init.d/50-rst.el
@@ -4,3 +4,9 @@
 
 (add-to-list 'auto-mode-alist '("\\.rst$" . rst-mode))
 (add-hook 'rst-mode-hook 'turn-on-auto-fill)
+
+(cond ((equal font-lock-global-modes t)
+       (setq font-lock-global-modes '(not rst-mode)))
+      ((and (listp font-lock-global-modes)
+            (equal (car font-lock-global-modes) 'not))
+       (append-to-list font-lock-global-modes 'rst-mode)))
