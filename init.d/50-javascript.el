1e9d5c272f1d5cdc8a83f7fc81f3c40b9071cd23
Petri Lehtinen <petri.lehtinen@inoi.fi>
2010-05-17 13:25:21 +0300

    Use espresso-mode for Javascript and JSON

--- init.d/50-javascript.el
+++ init.d/50-javascript.el
@@ -1,8 +1,3 @@
-;; (require 'js2-mode)
-
-;; (setq auto-mode-alist (cons '("\\.js$" . js2-mode) auto-mode-alist))
-
-;; (add-hook 'js2-mode-hook
-;;           #'(lambda ()
-;;               (setq js2-basic-offset 4)
-;;               (setq js2-mirror-mode nil)))
+(autoload #'espresso-mode "espresso" "Start espresso-mode" t)
+(add-to-list 'auto-mode-alist '("\\.js$" . espresso-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . espresso-mode))
