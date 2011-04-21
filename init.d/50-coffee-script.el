90af5ce81940b044a43c5f369e9ea9f1ab97d202
Petri Lehtinen <petri@digip.org>
2011-04-21 22:05:03 +0300

    Fix coffee-mode indentation

--- init.d/50-coffee-script.el
+++ init.d/50-coffee-script.el
@@ -2,6 +2,6 @@
 (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
 (add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
 
-(add-hook 'coffee-mode-hook
-          (lambda ()
-            (set (make-local-variable 'tab-width) 2)))
+(setq coffee-tab-width 2)
+(add-hook 'coffee-mode-hook #'(lambda ()
+  (set (make-local-variable 'tab-width) coffee-tab-width)))
