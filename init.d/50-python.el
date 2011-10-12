47e21f4f982cf7f207efda27df43ab528faa6172
Petri Lehtinen <petri.lehtinen@panic.fi>
2011-10-12 13:03:29 +0300

    Disable flymake in tramp buffers

--- init.d/50-python.el
+++ init.d/50-python.el
@@ -13,13 +13,14 @@
 
 (when (load "flymake" t)
   (defun flymake-pyflakes-init ()
-    (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
-           (local-file (file-relative-name
-                        temp-file
-                        (file-name-directory buffer-file-name))))
-      (list "~/.emacs-config/bin/pychecker.sh"
-            (list (expand-file-name virtualenv-dir) local-file))))
+    (when (not (subsetp (list (current-buffer)) (tramp-list-remote-buffers)))
+      (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                         'flymake-create-temp-inplace))
+             (local-file (file-relative-name
+                          temp-file
+                          (file-name-directory buffer-file-name))))
+        (list "~/.emacs-config/bin/pychecker.sh"
+              (list (expand-file-name virtualenv-dir) local-file)))))
   (add-to-list 'flymake-allowed-file-name-masks
                '("\\.py\\'" flymake-pyflakes-init))
 
