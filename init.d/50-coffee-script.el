b1b15211d857ada6589a04f32a3aa9e4c5d32964
Petri Lehtinen <petri.lehtinen@panic.fi>
2012-10-15 15:42:37 +0300

    Block indenting support to coffee-mode

--- init.d/50-coffee-script.el
+++ init.d/50-coffee-script.el
@@ -1,7 +1,11 @@
 (require 'coffee-mode)
-(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
-(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
-
 (setq coffee-tab-width 2)
-(add-hook 'coffee-mode-hook #'(lambda ()
-  (set (make-local-variable 'tab-width) coffee-tab-width)))
+
+(add-hook 'coffee-mode-hook
+  #'(lambda ()
+      (define-key coffee-mode-map (kbd "C-c C-;") 'coffee-indent-shift-left)
+      (define-key coffee-mode-map (kbd "C-c C-:") 'coffee-indent-shift-right)
+      (define-key coffee-mode-map (kbd "C-c C-c") 'comment-region)
+      (define-key coffee-mode-map (kbd "C-c C-u") 'uncomment-region)
+      (subword-mode)
+      (which-function-mode)))
