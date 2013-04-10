d2e8c896000fb00f90c0d820a2eadc4ded7ed08e
Petri Lehtinen <petri@digip.org>
2013-04-10 08:45:51 +0300

    Add git-gutter keys

--- init.d/50-git-gutter.el
+++ init.d/50-git-gutter.el
@@ -4,3 +4,5 @@
 (setq git-gutter:always-show-gutter t)
 
 (global-set-key (kbd "C-x v =") 'git-gutter:popup-diff)
+(global-set-key (kbd "C-x v n") 'git-gutter:next-hunk)
+(global-set-key (kbd "C-x v p") 'git-gutter:previous-hunk)
