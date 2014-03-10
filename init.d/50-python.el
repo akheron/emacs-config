d7fc7176781959629162f8dfef2db45a295797b7
Petri Lehtinen <petri@digip.org>
2014-03-10 14:45:53 +0200

    fixup

--- init.d/50-python.el
+++ init.d/50-python.el
@@ -9,6 +9,9 @@
       (define-key python-mode-map (kbd "C-c C-u") 'uncomment-region)
       (define-key python-mode-map (kbd "C-c C-n") 'flycheck-next-error)
       (define-key python-mode-map (kbd "C-c C-p") 'flycheck-prev-error)
+      (setq flycheck-flake8rc "~/.emacs-config/config/flake8.ini")
+      (setq flycheck-python-flake8-executable
+            (concat virtualenv-dir "/bin/flake8"))
       (electric-indent-local-mode -1)
       (subword-mode)
       (which-function-mode)))
--- init.d/90-flycheck.el
+++ init.d/90-flycheck.el
@@ -11,11 +11,6 @@
 (eval-after-load 'flycheck
   '(setq flycheck-checkers (delq 'emacs-lisp-checkdoc flycheck-checkers)))
 
-(global-flycheck-mode t)
-
-;; flake8 is in virtualenv-dir/bin
-(add-to-list 'exec-path (concat (expand-file-name virtualenv-dir) "/bin") t)
-
 (setq flycheck-display-errors-delay 0.1)
 
 (add-hook 'after-init-hook #'global-flycheck-mode)
