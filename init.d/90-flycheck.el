b67ce0cb370c7d5c5063e34b1e6bc6ed60546672
Petri Lehtinen <petri@digip.org>
2015-04-13 11:02:46 +0300

    Truncate flake8 output because flycheck is slow

--- init.d/90-flycheck.el
+++ init.d/90-flycheck.el
@@ -8,8 +8,11 @@
 
 (setq-default flycheck-flake8rc
               (expand-file-name "~/.emacs-config/conf/flake8rc"))
+
+;; Remember to copy ~/.emacs-config/bin/flake8trunc to the
+;; virtualenv's bin directory.
 (setq-default flycheck-python-flake8-executable
-              (concat (expand-file-name virtualenv-dir) "/bin/flake8"))
+              (concat (expand-file-name virtualenv-dir) "/bin/flake8trunc"))
 
 ;; Disable elisp checker.
 (eval-after-load 'flycheck
