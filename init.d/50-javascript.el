f130dafd571dc6da08d88afe42eb1ef55c237142
Petri Lehtinen <petri@digip.org>
2015-11-11 11:08:31 +0200

    Sanitize js2-mode setup, add JSON setup

--- init.d/50-javascript.el
+++ init.d/50-javascript.el
@@ -11,12 +11,15 @@
   (setq-default js2-strict-var-redeclaration-warning t)
   (setq-default js2-strict-var-hides-function-arg-warning t)
 
-  (add-hook 'js2-mode-hook
-    #'(lambda ()
-        (define-key js2-mode-map "\C-m" 'newline-and-indent)
-        (define-key js2-mode-map (kbd "C-c C-c") 'comment-region)
-        (define-key js2-mode-map (kbd "C-c C-u") 'uncomment-region)
-        (define-key js2-mode-map (kbd "C-c C-n") 'next-error)
-        (define-key js2-mode-map (kbd "C-c C-p") 'previous-error)
-        (setq js2-basic-offset 2)
-        (subword-mode))))
+  (bind-key "C-m" 'newline-and-indent js2-mode-map)
+  (bind-key "C-c C-c" 'comment-region js2-mode-map)
+  (bind-key "C-c C-u" 'uncomment-region js2-mode-map)
+  (bind-key "C-c C-n" 'flycheck-next-error js2-mode-map)
+  (bind-key "C-c C-p" 'flycheck-previous-error js2-mode-map)
+
+  (add-hook 'js2-mode-hook '(lambda ()
+    (setq js2-basic-offset 2)
+    (subword-mode))))
+
+; js-mode is used for JSON
+(add-hook 'js-mode-hook #'(lambda () (setq js-indent-level 2)))
