82bf55ce5d7b24827e76e4fe8b70cb5646948ed3
Petri Lehtinen <petri@digip.org>
2015-11-11 11:07:51 +0200

    Add elpa and marmalade package repos

--- init.d/01-package.el
+++ init.d/01-package.el
@@ -1,6 +1,9 @@
 (require 'package)
 
 (setq package-archives
-      '(("melpa-stable" . "https://stable.melpa.org/packages/")
-        ("melpa" . "https://melpa.org/packages/")))
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+        ("marmalade" . "https://marmalade-repo.org/packages/")
+        ("melpa-stable" . "https://stable.melpa.org/packages/")
+        ("melpa" . "http://melpa.org/packages/")))
+
 (package-initialize)
