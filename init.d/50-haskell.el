2b9e99448b9a9add27d577c46110694576d15386
Petri Lehtinen <petri@digip.org>
2018-01-14 20:45:31 +0200

    Use intero for haskell

--- init.d/50-haskell.el
+++ init.d/50-haskell.el
@@ -3,3 +3,8 @@
   :mode "\\.hs$"
   :config
   (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation))
+
+(use-package intero
+  :ensure t
+  :config
+  (add-hook 'haskell-model-hook 'intero-mode))
