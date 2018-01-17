ae80ee556a03064f82ba5b16c79baae51380606c
Petri Lehtinen <petri@digip.org>
2018-01-17 07:25:17 +0200

    Upgrade packages on startup

--- init.d/01-package.el
+++ init.d/01-package.el
@@ -2,8 +2,8 @@
 
 (setq package-archives
       '(("gnu" . "http://elpa.gnu.org/packages/")
-        ("marmalade" . "https://marmalade-repo.org/packages/")
         ("melpa-stable" . "https://stable.melpa.org/packages/")
         ("melpa" . "http://melpa.org/packages/")))
 
 (package-initialize)
+(package-refresh-contents)
