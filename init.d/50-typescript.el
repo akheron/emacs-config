cf04da186ae6acee0ab8737c111e9deb507cd98b
Petri Lehtinen <petri@digip.org>
2018-01-17 07:16:47 +0200

    Add typescript

---
+++ init.d/50-typescript.el
@@ -0,0 +1,19 @@
+(use-package typescript-mode
+  :ensure t
+  :mode "\\.ts\\'")
+
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+(use-package tide
+  :ensure t
+  :init (add-hook 'typescript-mode-hook #'setup-tide-mode))
