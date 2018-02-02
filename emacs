b8797534b3e40d146ddafb73c0d9ca8ddaf278de
Petri Lehtinen <petri.lehtinen@reaktor.com>
2018-02-02 13:49:05 +0200

    Ditch emacs-site.el

--- emacs
+++ emacs
@@ -8,9 +8,6 @@
 
 (setq default-directory "~/")
 
-(when (file-exists-p "~/.emacs-site.el")
-  (load "~/.emacs-site"))
-
 (dolist (file (directory-files "~/.emacs-config/init.d" t ".elc?$"))
   (load (file-name-sans-extension file)))
 
--- init.d/90-flycheck.el
+++ init.d/90-flycheck.el
@@ -9,25 +9,26 @@
   :ensure t
   :config
 
-  ;; Only have flycheck bitching in left-fringe
-  (setq flycheck-highlighting-mode 'lines)
+  (let ((virtualenv-dir "~/.virtualenvs/emacs"))
+    ;; Only have flycheck bitching in left-fringe
+    (setq flycheck-highlighting-mode 'lines)
 
-  ;; Use grizzl instead of ido for completion
-  (setq flycheck-completion-system 'grizzl)
+    ;; Use grizzl instead of ido for completion
+    (setq flycheck-completion-system 'grizzl)
 
-  (setq-default flycheck-flake8rc
-                (expand-file-name "~/.emacs-config/conf/flake8rc"))
+    (setq-default flycheck-flake8rc
+                  (expand-file-name "~/.emacs-config/conf/flake8rc"))
 
-  ;; Remember to copy ~/.emacs-config/bin/flake8trunc to the
-  ;; virtualenv's bin directory.
-  (setq-default flycheck-python-flake8-executable
-                (concat (expand-file-name virtualenv-dir) "/bin/flake8trunc"))
+    ;; Remember to copy ~/.emacs-config/bin/flake8trunc to the
+    ;; virtualenv's bin directory.
+    (setq-default flycheck-python-flake8-executable
+                  (concat (expand-file-name virtualenv-dir) "/bin/flake8trunc"))
 
-  ;; Disable elisp checker.
-  (setq flycheck-checkers (delq 'emacs-lisp-checkdoc flycheck-checkers))
+    ;; Disable elisp checker.
+    (setq flycheck-checkers (delq 'emacs-lisp-checkdoc flycheck-checkers))
 
-  ;; Don't use a config file for eslint
-  (setq flycheck-eslintrc nil)
+    ;; Don't use a config file for eslint
+    (setq flycheck-eslintrc nil)
 
-  (setq flycheck-display-errors-delay 0.1)
-  (add-hook 'after-init-hook #'global-flycheck-mode))
+    (setq flycheck-display-errors-delay 0.1)
+    (add-hook 'after-init-hook #'global-flycheck-mode)))
