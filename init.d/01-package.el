6ffa079b6d0c6540b5e8482aa54e0a8b1756e447
Petri Lehtinen <petri.lehtinen@reaktor.com>
2018-03-05 08:27:07 +0200

    Only use melpa

--- init.d/01-package.el
+++ init.d/01-package.el
@@ -1,9 +1,6 @@
 (require 'package)
 
 (setq package-archives
-      '(("gnu" . "http://elpa.gnu.org/packages/")
-        ("melpa-stable" . "https://stable.melpa.org/packages/")
-        ("melpa" . "http://melpa.org/packages/")))
+      '(("melpa" . "http://melpa.org/packages/")))
 
 (package-initialize)
-(package-refresh-contents)
